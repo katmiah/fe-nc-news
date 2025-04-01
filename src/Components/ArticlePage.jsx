f0423d15fb0c1b5c282d7885d803d3b73913fc9d
Katie Hardy <katiehardy@macbookpro.home>
2025-04-01 11:46:03 +0100

    added comments to associated article with little styling

--- src/Components/ArticlePage.jsx
+++ src/Components/ArticlePage.jsx
@@ -1,19 +1,29 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Comments from "./Comments";
 import axios from "axios";
 
 function ArticlePage() {
   const { article_id } = useParams();
-  const [article, setArticle] = useState();
+  const [article, setArticle] = useState([]);
+  const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState();
 
   useEffect(() => {
     setIsLoading(true);
-    axios
-      .get(`https://nc-news-pidx.onrender.com/api/articles/${article_id}`)
-      .then((response) => {
-        setArticle(response.data.article);
+
+    const fetchArticle = axios.get(
+      `https://nc-news-pidx.onrender.com/api/articles/${article_id}`
+    );
+    const fetchComments = axios.get(
+      `https://nc-news-pidx.onrender.com/api/articles/${article_id}/comments`
+    );
+
+    Promise.all([fetchArticle, fetchComments])
+      .then(([articleResponse, commentsResponse]) => {
+        setArticle(articleResponse.data.article);
+        setComments(commentsResponse.data.comments);
       })
       .catch((error) => {
         setError(error.message);
@@ -29,6 +39,9 @@ function ArticlePage() {
   if (error) {
     return <p>Error: {error}</p>;
   }
+  if (!article) {
+    return <p>No article found.</p>;
+  }
 
   return (
     <div className="article-content">
@@ -40,8 +53,19 @@ function ArticlePage() {
       <img src={article.article_img_url} alt="Image for each article" />
       <p>{article.body}</p>
       <p>Votes {article.votes}</p>
-      <p>Comments {article.comment_count}</p>
+
+      <h4>Comments ({comments.length})</h4>
+      <div>
+        {comments.length > 0 ? (
+          comments.map((comment) => (
+            <Comments key={comment.comment_id} comment={comment} />
+          ))
+        ) : (
+          <p>No comments yet. Be the first to comment!</p>
+        )}
+      </div>
     </div>
   );
 }
+
 export default ArticlePage;
